d08a4a86aff95024c92c727ae8c8fc67738a445d
Dirceu Garcia <70041892+Dirceug@users.noreply.github.com>
2024-03-08 20:36:51 -0300

    Add files via upload

---
+++ src/pages/login.js
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+import { SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        //Aqui implementamos a lógica de login
+        console.log(`Email: ${email},Password: ${password} `)
+    }
+
+    return (
+        <View style={styles.container} >
+            <TextInput
+                style={styles.input}
+                placeHolder= "E-mail"
+                value={email}
+                onChangeText={setEmail}
+                
+            />
+            <TextInput
+                style={styles.input}
+                placeHolder= "E-Senha"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin()}>
+                <Text style={styles.buttonText} >
+                    Entrar
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: '#fff'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 5,
+        padding: 10,
+        marginVertical: 10,
+        width: '80%'
+    },
+    button: {
+        backgroundColor: "#3498db",
+        borderRadius: 5,
+        padding: 10,
+        width: "80%",
+        alignItens: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
+
+export default Login;
